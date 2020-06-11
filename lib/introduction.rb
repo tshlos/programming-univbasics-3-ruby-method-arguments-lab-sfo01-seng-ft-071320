3d4e83538599896b7cef9e3035640d50da563e04
Taci Shlosberg <tshlosberg@gmail.com>
2020-06-11 18:44:25 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -19,12 +19,12 @@ end
 introduction_with_language("Taci", "Ruby")
 
 
-def introduction_with_language_optional(name, language)
+def introduction_with_language_optional(name)
     puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 
 introduction_with_language_optional("Taci" "Ruby")
-introduction_with_language_optional("Taci")
+
 
 
 
